75fd196ac8ba88fcaa4b87aa03308f9f71d2b3e4
Felipe Alves <felipe88alves@gmail.com>
2024-08-12 00:04:32 +0100

    feat: add client for List and Delete
     - decrease threshold for golangci-lint duplication check

--- client.go
+++ client.go
@@ -6,9 +6,24 @@ func (c *Cluster) Create(obj client.Object, opts ...client.CreateOption) error {
 	return c.client.Create(c.ctx, obj, opts...)
 }
 
+func (c *Cluster) Delete(obj client.Object, opts ...client.DeleteOption) func() error {
+	return func() error {
+		if err := c.Get(obj)(); err != nil {
+			return err
+		}
+		return c.client.Delete(c.ctx, obj, opts...)
+	}
+}
+
 func (c *Cluster) Get(obj client.Object, opts ...client.GetOption) func() error {
 	key := client.ObjectKeyFromObject(obj)
 	return func() error {
 		return c.client.Get(c.ctx, key, obj, opts...)
+	}
+}
+
+func (c *Cluster) List(obj client.ObjectList, opts ...client.ListOption) func() error {
+	return func() error {
+		return c.client.List(c.ctx, obj, opts...)
 	}
 }
--- client_test.go
+++ client_test.go
@@ -2,11 +2,13 @@ package kctest
 
 import (
 	"context"
+	"fmt"
 	"testing"
 
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apiextensions-apiserver/pkg/client/clientset/clientset/scheme"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"sigs.k8s.io/controller-runtime/pkg/client"
 )
 
 func setupK8sClient() (*Cluster, error) {
@@ -21,12 +23,20 @@ func setupK8sClient() (*Cluster, error) {
 	return c, nil
 }
 
-func setupConfigMap(name string) *corev1.ConfigMap {
+func setupConfigMap(name, ns string) *corev1.ConfigMap {
 	return &corev1.ConfigMap{
 		ObjectMeta: metav1.ObjectMeta{
 			Name:      "cm" + name,
-			Namespace: "default",
+			Namespace: ns,
 		}}
+}
+
+func setupNamespace(ns string) *corev1.Namespace {
+	return &corev1.Namespace{
+		ObjectMeta: metav1.ObjectMeta{
+			Name: ns,
+		},
+	}
 }
 
 func TestCreate(t *testing.T) {
@@ -60,7 +70,7 @@ func TestCreate(t *testing.T) {
 			t.Parallel()
 			cm := new(corev1.ConfigMap)
 			if tc.setupResource {
-				cm = setupConfigMap(tc.resourceName)
+				cm = setupConfigMap(tc.resourceName, "default")
 			}
 
 			err = c.Create(cm)
@@ -80,13 +90,13 @@ func TestGet(t *testing.T) {
 		expectedFail  bool
 	}{
 		{
-			name:          "Resource is created. Get succeeds.",
+			name:          "Resource exists. Get succeeds.",
 			resourceName:  "1",
 			setupResource: true,
 			expectedFail:  false,
 		},
 		{
-			name:          "Resource is not created. Get fails.",
+			name:          "Resource does not exist. Get fails.",
 			resourceName:  "2",
 			setupResource: false,
 			expectedFail:  true,
@@ -102,13 +112,119 @@ func TestGet(t *testing.T) {
 			t.Parallel()
 			cm := new(corev1.ConfigMap)
 			if tc.setupResource {
-				cm = setupConfigMap(tc.resourceName)
+				cm = setupConfigMap(tc.resourceName, "default")
 				if err = c.Create(cm); err != nil {
 					t.Fatalf("setup failed. Failed to create resource: %s", err.Error())
 				}
 			}
 
 			err := c.Get(cm)()
+			errResult := (err != nil)
+			if errResult != tc.expectedFail {
+				t.Fatalf("Expected to fail: %t, Result: %t, Error: %s", tc.expectedFail, errResult, err.Error())
+			}
+		})
+	}
+}
+
+func TestList(t *testing.T) {
+	scenarios := []struct {
+		name          string
+		resourceName  []string
+		setupResource bool
+		expectedFail  bool
+	}{
+		{
+			name:          "Resources exist. Get List succeeds.",
+			resourceName:  []string{"1", "3"},
+			setupResource: true,
+			expectedFail:  false,
+		},
+		{
+			name:          "Resources do not exist. Get List fails.",
+			resourceName:  []string{"2", "4"},
+			setupResource: false,
+			expectedFail:  true,
+		},
+	}
+	c, err := setupK8sClient()
+	if err != nil {
+		t.Fatalf("failed to setup kind cluster: %s", err.Error())
+	}
+	defer t.Cleanup(func() { _ = c.Stop() })
+	var nsId int
+	for _, tc := range scenarios {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			// setup NS
+			ns := setupNamespace(fmt.Sprintf("ns-%d", nsId))
+			nsId++
+			if err := c.Create(ns); err != nil {
+				t.Fatalf("setup failed. Failed to create namespace: %s", err.Error())
+			}
+			if tc.setupResource {
+				// setup CM
+				for _, r := range tc.resourceName {
+					cm := setupConfigMap(r, ns.GetName())
+					if err := c.Create(cm); err != nil {
+						t.Fatalf("setup failed. Failed to create resource: %s", err.Error())
+					}
+				}
+			}
+
+			cmList := new(corev1.ConfigMapList)
+			opts := []client.ListOption{client.InNamespace(ns.GetName())}
+			err := c.List(cmList, opts...)()
+			errResult := (err != nil)
+			if errResult != tc.expectedFail {
+				return
+			}
+			if !tc.expectedFail {
+				if len(cmList.Items) != len(tc.resourceName) {
+					t.Fatalf("Length of Resources in List don't match: Expected: %d, Actual: %d", len(tc.resourceName), len(cmList.Items))
+				}
+			}
+		})
+	}
+}
+
+func TestDelete(t *testing.T) {
+	scenarios := []struct {
+		name          string
+		resourceName  string
+		setupResource bool
+		expectedFail  bool
+	}{
+		{
+			name:          "Resource exists. Delete succeeds.",
+			resourceName:  "1",
+			setupResource: true,
+			expectedFail:  false,
+		},
+		{
+			name:          "Resource does not exist. Delete fails.",
+			resourceName:  "2",
+			setupResource: false,
+			expectedFail:  true,
+		},
+	}
+	c, err := setupK8sClient()
+	if err != nil {
+		t.Fatalf("failed to setup kind cluster: %s", err.Error())
+	}
+	defer t.Cleanup(func() { _ = c.Stop() })
+	for _, tc := range scenarios {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+			cm := new(corev1.ConfigMap)
+			if tc.setupResource {
+				cm = setupConfigMap(tc.resourceName, "default")
+				if err = c.Create(cm); err != nil {
+					t.Fatalf("setup failed. Failed to create resource: %s", err.Error())
+				}
+			}
+
+			err := c.Delete(cm)()
 			errResult := (err != nil)
 			if errResult != tc.expectedFail {
 				t.Fatalf("Expected to fail: %t, Result: %t, Error: %s", tc.expectedFail, errResult, err.Error())
